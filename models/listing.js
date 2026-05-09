9ffc5772f6ee64282274e625ce4ecfbe0e7d2055
yuvrajsinh jadeja <yuvrajsinhjadeja25023@gmail.com>
2026-05-09 18:31:05 +0530

    Refactor listing schema and model definition

--- models/listing.js
+++ models/listing.js
@@ -1,24 +1,24 @@
 const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
+const Schema=mongoose.Schema();
 
-const listing=new Schema({
+const dSchema=new mongoose.Schema({
     title:{
         type:String,
         require:true
     },
+    image:{
+        filename:{
+            type:String,
+            default:"image link"
+        },
+        url:{
+            type:String,
+            default:"https://i.pinimg.com/1200x/98/8a/67/988a677388a2357b96a31bc2c53c9db3.jpg"
+        }
+    },
     description:{
         type:String,
         require:true
-    },
-    image:{filename:{
-        type:String,
-        default:"listing image",
-    },
-     url: {
-            type: String,
-            default:
-            "https://unsplash.com/photos/a-large-couch-sitting-on-top-of-a-patio-wnMnoSXvn1s"
-        }
     },
     price:{
         type:Number,
@@ -33,5 +33,6 @@ const listing=new Schema({
         require:true
     }
 })
-const List=mongoose.model("List",listing)
+
+const List=mongoose.model("List",dSchema)
 module.exports=List;
