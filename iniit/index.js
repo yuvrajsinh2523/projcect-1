32440a9f52179a4a9a759524d6f8d31881a3890d
yuvrajsinh jadeja <yuvrajsinhjadeja25023@gmail.com>
2026-05-09 18:30:37 +0530

    Refactor database initialization and connection logic

--- iniit/index.js
+++ iniit/index.js
@@ -1,26 +1,20 @@
 const mongoose=require("mongoose");
+const data=require("./data.js")
+const List=require('../models/listing.js')
 
-const initData=require("./data.js");
-const Listing=require("../models/listing.js");
-
-const MONGO_url="mongodb://127.0.0.1:27017/wondestlust";
+const MONGO_URL="mongodb://127.0.0.1:27017/bnb";
 
 main().then((res)=>{
-    console.log("listing done");
+    console.log("connected");
 }).catch((err)=>{
     console.log(err);
-});
-
+})
 async function main(){
-    await mongoose.connect(MONGO_url);
-}
- 
-
-const initDB = async ()=>{
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("data inisialize");
-
+    await mongoose.connect(MONGO_URL);
 }
 
-initDB();
+const enterDB=async ()=>{
+    await List.deleteMany();
+    await List.insertMany(data.data);
+}
+enterDB();
